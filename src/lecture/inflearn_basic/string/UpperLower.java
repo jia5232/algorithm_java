2440e8430c4cecbb5b7524d4c12daf97911b9107
jia5232 <jia5232@naver.com>
2023-08-01 16:46:41 +0900

    string: UpperLower2

--- src/lecture/inflearn_basic/string/UpperLower.java
+++ src/lecture/inflearn_basic/string/UpperLower.java
@@ -3,14 +3,22 @@ import java.util.Scanner;
 
 public class UpperLower {
     public static String solution(String str){
-        String[] arr = str.split("");
         String result = "";
-        for(String s : arr){
-            if(Character.isLowerCase(s.charAt(0))){
-                result+=s.toUpperCase();
-            }else{
-                result+=s.toLowerCase();
-            }
+
+        //1. 대소문자 구분 메소드 사용
+//        String[] arr = str.split("");
+//        for(String s : arr){
+//            if(Character.isLowerCase(s.charAt(0))){
+//                result+=s.toUpperCase();
+//            }else{
+//                result+=s.toLowerCase();
+//            }
+//        }
+
+        //2. 아스키코드 사용 (65~90:대문자, 97~122:소문자)
+        for(char c : str.toCharArray()){
+            if(c>=65 && c<=90) result+=Character.toLowerCase(c);
+            else if(c>=97 && c<=122) result+=Character.toUpperCase(c);
         }
         return result;
     }
