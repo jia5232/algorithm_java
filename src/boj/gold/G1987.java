9f8522418a2b5816bdd2e5d0ded6ec6af72c2460
jia5232 <jia5232@naver.com>
2024-01-11 23:49:01 +0900

    20240111

--- src/boj/gold/G1987.java
+++ src/boj/gold/G1987.java
@@ -3,31 +3,28 @@ package boj.gold;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Arrays;
 import java.util.HashSet;
 import java.util.Set;
 import java.util.StringTokenizer;
 
 public class G1987 {
     static char[][] board;
-    static int[][] max;
     static Set<Character> record;
     static int row, col;
-
+    static int result;
     static int[][] dis = {{0, -1}, {-1, 0}, {0, 1}, {1, 0}};
 
-    public static void DFS(int r, int c){
+    public static void DFS(int r, int c, int sum){
+        result = Math.max(sum, result);
         for (int i = 0; i < 4; i++) {
             int nextR = r+dis[i][0];
             int nextC = c+dis[i][1];
             if(nextR>=0 && nextR<row && nextC>=0 && nextC<col && !record.contains(board[nextR][nextC])){
                 record.add(board[nextR][nextC]);
-                max[nextR][nextC] = max[r][c]+1;
-                DFS(nextR, nextC);
+                DFS(nextR, nextC, sum+1);
                 record.remove(board[nextR][nextC]);
             }
         }
-
     }
 
     public static void main(String[] args) throws IOException {
@@ -36,7 +33,6 @@ public class G1987 {
         row = Integer.parseInt(st.nextToken());
         col = Integer.parseInt(st.nextToken());
         board = new char[row][col];
-        max = new int[row][col];
         record = new HashSet<>();
         for (int i = 0; i < row; i++) {
             char[] tmp = br.readLine().toCharArray();
@@ -44,16 +40,10 @@ public class G1987 {
                 board[i][j] = tmp[j];
             }
         }
-        max[0][0] = 1;
+        result = 0;
         record.add(board[0][0]);
-        DFS(0, 0);
-        for (int i = 0; i < row; i++) {
-            System.out.println(Arrays.toString(max[i]));
-        }
-        int maxValue = Integer.MIN_VALUE;
-        for (int i = 0; i < row; i++) {
-            maxValue = Math.max(maxValue, Arrays.stream(max[i]).max().getAsInt());
-        }
-        System.out.println(maxValue);
+        DFS(0, 0, 1);
+
+        System.out.println(result);
     }
 }
