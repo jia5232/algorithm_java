f537795afd54ce7d7c1aa9696aa33d9577898c87
Jia Kwon <jia5232@kookmin.ac.kr>
2024-08-14 18:13:20 +0900

    20240814

--- src/lecture/inflearn_basic/array/VisibleStudent.java
+++ src/lecture/inflearn_basic/array/VisibleStudent.java
@@ -3,27 +3,22 @@ package lecture.inflearn_basic.array;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.StringTokenizer;
 
 public class VisibleStudent {
-    public static int solution(int[] arr){
-        int answer = 1;
-        int std = arr[0];
-        for (int i = 1; i < arr.length; i++) {
-            if(arr[i] > std){
-                answer++;
-                std = arr[i];
-            }
-        }
-        return answer;
-    }
-
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int num = Integer.parseInt(br.readLine());
         int[] arr = new int[num];
-        StringTokenizer st = new StringTokenizer(br.readLine());
-        for (int i = 0; i < num; i++) arr[i] = Integer.parseInt(st.nextToken());
-        System.out.println(solution(arr));
+        String[] inputArr = br.readLine().split(" ");
+        for (int i = 0; i < num; i++) arr[i] = Integer.parseInt(inputArr[i]);
+        int answer = 1;
+        int beforeMax = arr[0];
+        for (int i = 1; i < num; i++) {
+            if(arr[i]>beforeMax) {
+                answer++;
+                beforeMax = arr[i];
+            }
+        }
+        System.out.println(answer);
     }
 }
