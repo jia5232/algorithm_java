c687604ae9b9eff8861235d3f637495e61518b21
Jia Kwon <jia5232@kookmin.ac.kr>
2024-09-03 14:16:32 +0900

    20240903

--- src/lecture/inflearn_basic/dynamicprogramming/MaxScore.java
+++ src/lecture/inflearn_basic/dynamicprogramming/MaxScore.java
@@ -5,39 +5,20 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
-//public class MaxScore {
-//    public static void main(String[] args) throws IOException {
-//        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-//        StringTokenizer st;
-//        st = new StringTokenizer(br.readLine());
-//        int n = Integer.parseInt(st.nextToken());
-//        int m = Integer.parseInt(st.nextToken()); //제한 시간
-//        int[] dy = new int[m+1]; //시간 j까지 얻을 수 있는 최대 점수
-//        for (int i = 0; i < n; i++) {
-//            st = new StringTokenizer(br.readLine());
-//            int ps = Integer.parseInt(st.nextToken());
-//            int pt = Integer.parseInt(st.nextToken());
-//            for (int j = m; j >= pt; j--) {
-//                dy[j] = Math.max(dy[j], dy[j-pt]+ps);
-//            }
-//        }
-//        System.out.println(dy[m]);
-//    }
-//}
-
 public class MaxScore {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine());
+        StringTokenizer st;
+        st = new StringTokenizer(br.readLine());
         int n = Integer.parseInt(st.nextToken());
-        int m = Integer.parseInt(st.nextToken());
-        int[] dy = new int[m+1];
+        int m = Integer.parseInt(st.nextToken()); //제한 시간
+        int[] dy = new int[m+1]; //시간 j까지 얻을 수 있는 최대 점수
         for (int i = 0; i < n; i++) {
             st = new StringTokenizer(br.readLine());
-            int s = Integer.parseInt(st.nextToken());
-            int t = Integer.parseInt(st.nextToken());
-            for (int j = m; j >= t; j--) {
-                dy[j] = Math.max(dy[j], dy[j-t]+s);
+            int ps = Integer.parseInt(st.nextToken());
+            int pt = Integer.parseInt(st.nextToken());
+            for (int j = m; j >= pt; j--) {
+                dy[j] = Math.max(dy[j], dy[j-pt]+ps);
             }
         }
         System.out.println(dy[m]);
--- src/lecture/inflearn_basic/dynamicprogramming/MaximumPartialIncreasingSequence.java
+++ src/lecture/inflearn_basic/dynamicprogramming/MaximumPartialIncreasingSequence.java
@@ -10,11 +10,11 @@ public class MaximumPartialIncreasingSequence{
 
     public static int solution(int[] arr){
         int answer = 0;
-        dy = new int[arr.length]; //그 숫자를 마지막으로 하는 최대 부분 증가수열의 길이
+        dy = new int[arr.length];
         dy[0] = 1;
         for (int i = 1; i < arr.length; i++) {
             int max = 0;
-            for (int j = i-1; j>=0; j--) {
+            for (int j = i-1; j >= 0; j--) {
                 if(arr[j]<arr[i] && dy[j]>max) max = dy[j];
             }
             dy[i] = max+1;
