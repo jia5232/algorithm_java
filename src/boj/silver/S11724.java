987bfa8df68597884663940e2761f80c990a1b74
Jia Kwon <jia5232@kookmin.ac.kr>
2024-09-25 20:15:13 +0900

    20240925

--- src/boj/silver/S11724.java
+++ src/boj/silver/S11724.java
@@ -4,43 +4,29 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
-import java.util.StringTokenizer;
 
 public class S11724 {
+    static int n, m;
     static int[] ch;
-    static int answer;
+    static ArrayList<ArrayList<Integer>> graph = new ArrayList<>();
 
-    static ArrayList<ArrayList<Integer>> arrayList;
-
-    public static void DFS(int v){
-        if(ch[v]==0){
-            ch[v] = 1;
-            for (Integer i : arrayList.get(v)) {
-                if(ch[i]==0){
-                    DFS(i);
-                }
-            }
-        } else return;
-    }
-
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) throws IOException{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine());
-        int n = Integer.parseInt(st.nextToken());
-        int m = Integer.parseInt(st.nextToken());
-        arrayList = new ArrayList<>();
+        String[] inputArr = br.readLine().split(" ");
+        n = Integer.parseInt(inputArr[0]);
+        m = Integer.parseInt(inputArr[1]);
         for (int i = 0; i <= n; i++) {
-            arrayList.add(new ArrayList<>());
+            graph.add(new ArrayList<>());
         }
         for (int i = 0; i < m; i++) {
-            st = new StringTokenizer(br.readLine());
-            int a = Integer.parseInt(st.nextToken());
-            int b = Integer.parseInt(st.nextToken());
-            arrayList.get(a).add(b);
-            arrayList.get(b).add(a);
+            inputArr = br.readLine().split(" ");
+            int a = Integer.parseInt(inputArr[0]);
+            int b = Integer.parseInt(inputArr[1]);
+            graph.get(a).add(b);
+            graph.get(b).add(a);
         }
         ch = new int[n+1];
-        answer = 0;
+        int answer = 0;
         for (int i = 1; i <= n; i++) {
             if(ch[i]==0){
                 answer++;
@@ -48,5 +34,16 @@ public class S11724 {
             }
         }
         System.out.println(answer);
+    }
+
+    public static void DFS(int n){
+        if(ch[n]==1){
+            return;
+        }else{
+            ch[n] = 1;
+            for(int i : graph.get(n)){
+                DFS(i);
+            }
+        }
     }
 }
