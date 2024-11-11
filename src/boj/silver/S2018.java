a28241af6dcb717a2bc5954121383810bff9c8ea
Jia Kwon <jia5232@kookmin.ac.kr>
2024-11-11 14:51:35 +0900

    20241111

--- src/boj/silver/S2018.java
+++ src/boj/silver/S2018.java
@@ -5,26 +5,23 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 
 public class S2018 { //수들의 합 5
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) throws IOException{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int n = Integer.parseInt(br.readLine());
-        int answer = 1; // n 자기자신
-        int start = 1, end = 1, tmp = 1;
-        while (start <= n/2){
-            if(tmp==n){
-                answer++;
+        int start = 1, end = 1, sum = 1, count = 1;
+        while (end<n){
+            if(sum==n){
+                count++;
                 end++;
-                tmp += end;
-            }
-            else if(tmp>n){
-                tmp -= start;
+                sum+=end;
+            } else if(sum<n){
+                end++;
+                sum+=end;
+            } else{
+                sum-=start;
                 start++;
             }
-            else{
-                end++;
-                tmp += end;
-            }
         }
-        System.out.println(answer);
+        System.out.println(count);
     }
 }
--- src/boj/workbook/sorting/SortingAlgorithms.java
+++ src/boj/workbook/sorting/SortingAlgorithms.java
@@ -50,6 +50,88 @@ public class SortingAlgorithms {
         }
     }
 
+    public static void heapSort(int[] arr) {
+        int n = arr.length;
+
+        // 힙 구성
+        for (int i = n / 2 - 1; i >= 0; i--)
+            heapify(arr, n, i);
+
+        // 힙에서 하나씩 추출
+        for (int i = n - 1; i > 0; i--) {
+            int temp = arr[0];
+            arr[0] = arr[i];
+            arr[i] = temp;
+            heapify(arr, i, 0);
+        }
+    }
+
+    private static void heapify(int[] arr, int n, int i) {
+        int largest = i;
+        int left = 2 * i + 1;
+        int right = 2 * i + 2;
+
+        if (left < n && arr[left] > arr[largest])
+            largest = left;
+
+        if (right < n && arr[right] > arr[largest])
+            largest = right;
+
+        if (largest != i) {
+            int swap = arr[i];
+            arr[i] = arr[largest];
+            arr[largest] = swap;
+            heapify(arr, n, largest);
+        }
+    }
+
+    public static void mergeSort(int[] arr, int l, int r) {
+        if (l < r) {
+            int m = (l + r) / 2;
+            mergeSort(arr, l, m);
+            mergeSort(arr, m + 1, r);
+            merge(arr, l, m, r);
+        }
+    }
+
+    private static void merge(int[] arr, int l, int m, int r) {
+        int n1 = m - l + 1;
+        int n2 = r - m;
+
+        int[] L = new int[n1];
+        int[] R = new int[n2];
+
+        for (int i = 0; i < n1; ++i)
+            L[i] = arr[l + i];
+        for (int j = 0; j < n2; ++j)
+            R[j] = arr[m + 1 + j];
+
+        int i = 0, j = 0;
+        int k = l;
+        while (i < n1 && j < n2) {
+            if (L[i] <= R[j]) {
+                arr[k] = L[i];
+                i++;
+            } else {
+                arr[k] = R[j];
+                j++;
+            }
+            k++;
+        }
+
+        while (i < n1) {
+            arr[k] = L[i];
+            i++;
+            k++;
+        }
+
+        while (j < n2) {
+            arr[k] = R[j];
+            j++;
+            k++;
+        }
+    }
+
     public static int partition(int[] arr, int low, int high) {
         int pivot = arr[high];
         int i = low - 1;
@@ -75,8 +157,8 @@ public class SortingAlgorithms {
         insertionSort(arr.clone());
         selectionSort(arr.clone());
         quickSort(arr.clone(), 0, arr.length-1);
-//        heapSort(arr.clone());
-//        mergeSort(arr.clone(), 0, arr.length-1);
+        heapSort(arr.clone());
+        mergeSort(arr.clone(), 0, arr.length-1);
     }
 
     // 배열 출력 유틸리티 메소드
